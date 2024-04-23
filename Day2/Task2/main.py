4285b1e22ec5baba1253ff260eb1d209ec0a7d3a
Abhijith717 <abhijithnagula@gmail.com>
2024-04-23 21:34:52 +0530

    thirdCommit

--- Day2/Task2/main.py
+++ Day2/Task2/main.py
@@ -9,9 +9,10 @@ def guess_the_number():
             if(machine==user):
                 if(attempts>1):
                     print(f"Hurrah! You have guessed the correct number in {attempts} attempts")
+                    break
                 elif(attempts==1):
                     print(f"Hurrah! You have guessed the correct number in Single attempt")
-                break
+                    break
             elif(machine>user):
                 attempts+=1
                 print(f"You have to guess a number greater than {user}")
@@ -26,49 +27,45 @@ def guess_the_number():
 # guess_the_number()
 def Rock_Papper_Scizzor():
     try:
-        decision=input("Do you want to play (yes/no)")
-        while(decision.lower()=="yes"):
+        while(True):
             elements=["Rock","Papper","Scizzor"]
             machine=random.choice(elements)
             user=input("Rock Papper Scizzor ").lower()
             if(machine.lower()==user):
                 print(f"machine:{machine}\nuser:{user}\nThat was a tough call its a Tie")
-                Rock_Papper_Scizzor()
             elif((machine.lower()=="rock") and (user=="scizzor") ):
                 print(f"machine:{machine}\nuser:{user}\n Hah! I Won this time ")
-                Rock_Papper_Scizzor()
             elif((machine.lower()=="papper") and (user=="rock") ):
                 print(f"machine:{machine}\nuser:{user}\n Hah! I Won this time ")
-                Rock_Papper_Scizzor()
             elif((machine.lower()=="scizzor") and (user=="papper") ):
                 print(f"machine:{machine}\nuser:{user}\n Hah! I Won this time ")
-                Rock_Papper_Scizzor()
             else:
                 print(f"machine:{machine}\nuser:{user}\n You have won")
-                Rock_Papper_Scizzor()
-        else:
-            print("thankyou for playing")
-            main()
+            play_again = input("Do you want to play again? (yes/no) ").lower()
+            if play_again != "yes":
+                break
     except:
         print("please give the input according to the instructions you get\n Thankyou")
     
 
-print("Welcome just type 1 or 2 to play the desired game or 3 to exit")
+
 def main():
-    print("If U dont want to play just type 'no'")
-    play=int(input("Which game U want to play from the following\n1.Number Guessing\n2.Rock Papper Scizzor\n3.exit\n"))
-    if(play==1):
-        guess_the_number()
-        n=input("Do u want to play again from start yes/no")
-        if(n.lower()=="yes"):
-            main()
-    elif(play==2):
-        Rock_Papper_Scizzor()
-        n=input("Do u want to play again start yes/no")
-        if(n.lower()=="yes"):
-            main()
-    else:
-        print("Thankyou for playing the game")
+    print("Welcome! Choose a game to play:")
+    while True:
+        try:
+            choice = int(input("1. Number Guessing\n2. Rock Paper Scissors\n3. Exit\nEnter your choice: "))
+            if choice == 1:
+                guess_the_number()
+            elif choice == 2:
+                Rock_Papper_Scizzor()
+            elif choice == 3:
+                print("Thank you for playing!")
+                break
+            else:
+                print("Invalid choice! Please enter 1, 2, or 3.")
+        except:
+            print("please enter the correct values")
+    
 main()
 
     
